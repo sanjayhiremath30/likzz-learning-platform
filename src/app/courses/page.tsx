c0b88c5fabfd2439de689d9e54585d3d678000e7
sanjayhiremath30 <sanjay30.cse.rymec@gmail.com>
2026-03-15 22:32:30 +0530

    fix courses prerender error

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -1,6 +1,8 @@
 import { Suspense } from "react";
 import CoursesContent from "./CoursesContent";
 
+export const dynamic = "force-dynamic";
+
 export default function CoursesPage() {
     return (
         <Suspense fallback={<div className="p-10 text-center">Loading courses...</div>}>

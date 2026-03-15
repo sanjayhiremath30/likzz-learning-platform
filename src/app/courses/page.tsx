d485ab67ac4ff6980f58e7efd37e841e50a3b2d4
sanjayhiremath30 <sanjay30.cse.rymec@gmail.com>
2026-03-15 22:39:17 +0530

    fix courses prerender issue

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -2,6 +2,7 @@ import { Suspense } from "react";
 import CoursesContent from "./CoursesContent";
 
 export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
 
 export default function CoursesPage() {
     return (
